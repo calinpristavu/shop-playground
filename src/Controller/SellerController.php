ceca71ef45a3067ab8bb5216e3f4911e6115072c
Calin Pristavu <calinpristavu@gmail.com>
2019-07-19 16:17:39 +0300

    Rudimentary seller, creates random product

--- src/Controller/SellerController.php
+++ src/Controller/SellerController.php
@@ -4,12 +4,47 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Product\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Sylius\Component\Product\Factory\ProductFactoryInterface;
+use Sylius\Component\Product\Model\ProductInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class SellerController
+class SellerController extends AbstractController
 {
-    public function index()
-    {
-        return new Response('<body>seller</body>');
+    /**
+     * @Template("seller_dashboard.html.twig")
+     */
+    public function dashboard(
+        ProductRepositoryInterface $productRepo
+    ): array {
+        return [
+            'products' => $productRepo->findBy(['seller' => $this->getUser()])
+        ];
+    }
+
+    public function createProduct(
+        ProductFactoryInterface $productFactory,
+        ProductRepositoryInterface $productRepo
+    ) {
+        /** @var Product $product */
+        $product = $productFactory->createNew();
+
+        $user = $this->getUser();
+        $dateTime = new \DateTime();
+        $product->setName(sprintf(
+            'created by %s at %s',
+            $user->getUsername(),
+            $dateTime->format('c')
+        ));
+        $product->setCode((string) $dateTime->getTimestamp());
+        $product->setSlug((string) $dateTime->getTimestamp());
+
+        $product->setSeller($this->getUser());
+
+        $productRepo->add($product);
+
+        return $this->redirectToRoute('app_seller_dashboard');
     }
 }
